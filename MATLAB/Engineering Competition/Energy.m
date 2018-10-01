9d5abbadc2f66a0d09e35a94818c365c6664080c
camka14 <camka14@gmail.com>
2018-10-01 09:25:15 -0600

    Moved files

--- MATLAB/Engineering Competition/Energy.m
+++ MATLAB/Engineering Competition/Energy.m
@@ -1,15 +1,16 @@
 function [ x ] = Energy( Px,Py,k,m,mu,h1 )
-%input a k value for the spring, an x value for the distance pulled, a mass
-% and a hieght.
-x=.1; %m
+%input x and y coordingates in feet, k value for the spring in N/m, mu
+%mass of projectile in grams, value for friction constant, and initial
+%height
+x=.1; %m initial distance pulled
 D=1; %m 
-P=sqrt(Px^2+Py^2)*.3048 %m distance to target
+P=sqrt(Px^2+Py^2)*.3048; %m distance to target
 w=m*9.81; %N weight of payload
 Ff=mu*w*sind(45); %N force of friction
 while abs(D-P)>0.01
-    x=x+.0001; %m
+    x=x+.0001; %m distance to pull
     g=9.81; %m/s^2
-    h2=h1-x*sind(45); %m height during initial energy
+    h2=h1-x*sind(45); %m height at initial
     E=.5*k*x^2+m*g*h2-Ff*x; %J initial energy
     vi=sqrt(2*(E/m-g*h1)); %m/s velocity
     Vyi=vi*sind(45); %m/s vertical velocity
@@ -17,6 +18,8 @@ while abs(D-P)>0.01
     t=(-Vyi-sqrt(Vyi^2+2*g*h1))/-g; %s time
     D=Vxi*t; %m distance launched
 end
-theta=asind(Px/Py)
-D
+theta=asind(Px/Py);
+fprintf('Distance to target %.3f m\n', D)
+fprintf('Target heading %.3f deg\n', theta)
+fprintf('Draw Distance %.3f cm\n', x*100)
 end
